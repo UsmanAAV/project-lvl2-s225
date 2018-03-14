ab0427e2a228d3c693f82ca82125f089a4463dee
UsmanAAV <usman.a.a.v@gmail.com>
2018-03-15 01:13:53 +0500

    added tests for 'yaml' format (step2)

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -2,8 +2,14 @@ import genDiff from '../src';
 
 const fs = require('fs');
 
-test('genDiff', () => {
+test('genDiff-json', () => {
   const expected = fs.readFileSync('./__tests__/__fixtures__/expected.txt', 'utf8');
   expect(genDiff('./__tests__/__fixtures__/before.json', './__tests__/__fixtures__/after.json'))
+    .toBe(expected);
+});
+
+test('genDiff-yaml', () => {
+  const expected = fs.readFileSync('./__tests__/__fixtures__/expected.txt', 'utf8');
+  expect(genDiff('./__tests__/__fixtures__/before.yml', './__tests__/__fixtures__/after.yaml'))
     .toBe(expected);
 });
