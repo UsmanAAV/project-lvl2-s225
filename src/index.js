400bbed39b478eee0a314f7df3c211d1dab97cba
UsmanAAV <usman.a.a.v@gmail.com>
2018-03-15 09:35:34 +0500

    added parsers.js, rewrited index.js

--- src/index.js
+++ src/index.js
@@ -1,27 +1,34 @@
 import fs from 'fs';
+import path from 'path';
+import getParser from './parsers';
+
+const convertFileToObject = (pathToFile) => {
+  const ext = path.extname(pathToFile);
+  const data = fs.readFileSync(pathToFile);
+  const parser = getParser(ext);
+  return parser.parse(data);
+};
 
 const genDiff = (pathToBefore, pathToAfter) => {
-  const beforeFile = fs.readFileSync(pathToBefore);
-  const afterFile = fs.readFileSync(pathToAfter);
-  const beforeJSON = JSON.parse(beforeFile);
-  const afterJSON = JSON.parse(afterFile);
+  const beforeConfig = convertFileToObject(pathToBefore);
+  const afterConfig = convertFileToObject(pathToAfter);
 
-  const objKeys = Object.keys(beforeJSON).concat(Object.keys(afterJSON));
+  const objKeys = Object.keys(beforeConfig).concat(Object.keys(afterConfig));
   const keys = Array.from(new Set(objKeys));
 
   const resultArray = keys.reduce(
     (acc, key) => {
-      if (beforeJSON[key] === afterJSON[key]) {
-        return acc.concat(`    ${key}: ${beforeJSON[key]}`);
+      if (beforeConfig[key] === afterConfig[key]) {
+        return acc.concat(`    ${key}: ${beforeConfig[key]}`);
       }
-      if (key in beforeJSON && key in afterJSON && beforeJSON[key] !== afterJSON[key]) {
-        return acc.concat(`  + ${key}: ${afterJSON[key]}`).concat(`  - ${key}: ${beforeJSON[key]}`);
+      if (key in beforeConfig && key in afterConfig && beforeConfig[key] !== afterConfig[key]) {
+        return acc.concat(`  + ${key}: ${afterConfig[key]}`).concat(`  - ${key}: ${beforeConfig[key]}`);
       }
-      if (key in beforeJSON && !(key in afterJSON)) {
-        return acc.concat(`  - ${key}: ${beforeJSON[key]}`);
+      if (key in beforeConfig && !(key in afterConfig)) {
+        return acc.concat(`  - ${key}: ${beforeConfig[key]}`);
       }
-      if (!(key in beforeJSON) && key in afterJSON) {
-        return acc.concat(`  + ${key}: ${afterJSON[key]}`);
+      if (!(key in beforeConfig) && key in afterConfig) {
+        return acc.concat(`  + ${key}: ${afterConfig[key]}`);
       }
       return acc;
     },
