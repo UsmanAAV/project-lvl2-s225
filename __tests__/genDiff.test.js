d6d0191de0baf4e560cb2cd596cf83e667f8649d
UsmanAAV <usman.a.a.v@gmail.com>
2018-03-17 10:21:06 +0500

    added tests for 'plain' output

--- __tests__/genDiff.test.js
+++ __tests__/genDiff.test.js
@@ -37,3 +37,21 @@ test('genDiff-ini-recursive', () => {
   expect(genDiff('./__tests__/__fixtures__/before-recursive.ini', './__tests__/__fixtures__/after-recursive.ini'))
     .toBe(expected);
 });
+
+test('genDiff-json-recursive-plain', () => {
+  const expected = fs.readFileSync('./__tests__/__fixtures__/expected-recursive-plain.txt', 'utf8');
+  expect(genDiff('./__tests__/__fixtures__/before-recursive.json', './__tests__/__fixtures__/after-recursive.json', 'plain'))
+    .toBe(expected);
+});
+
+test('genDiff-yaml-recursive-plain', () => {
+  const expected = fs.readFileSync('./__tests__/__fixtures__/expected-recursive-plain.txt', 'utf8');
+  expect(genDiff('./__tests__/__fixtures__/before-recursive.yml', './__tests__/__fixtures__/after-recursive.yaml', 'plain'))
+    .toBe(expected);
+});
+
+test('genDiff-ini-recursive-plain', () => {
+  const expected = fs.readFileSync('./__tests__/__fixtures__/expected-recursive-plain.txt', 'utf8');
+  expect(genDiff('./__tests__/__fixtures__/before-recursive.ini', './__tests__/__fixtures__/after-recursive.ini', 'plain'))
+    .toBe(expected);
+});
