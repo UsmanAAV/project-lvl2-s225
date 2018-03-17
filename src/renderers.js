73cc27dbd98c4b3090b08dc4db7df1dc98a0e606
UsmanAAV <usman.a.a.v@gmail.com>
2018-03-17 10:22:07 +0500

    added renderPlain function for 'plain' output format

--- src/renderers.js
+++ src/renderers.js
@@ -1,3 +1,5 @@
+import fp from 'lodash/fp';
+
 const renderJSON = (ast, depth = 0) => {
   const makeIndent = count => `${'  '.repeat(count * 2)}`;
 
@@ -24,7 +26,36 @@ const renderJSON = (ast, depth = 0) => {
   return `{\n${result}\n${makeIndent(depth)}}`;
 };
 
+const renderPlain = (ast, prefix = '') => {
+  const printValue = (value, pref = '') => {
+    if (value instanceof Object) {
+      return 'complex value';
+    }
+    return `${pref}'${value}'`;
+  };
+  const propertyActions = {
+    added: elem => `Property '${prefix}${elem.keyName}' was added with ${printValue(elem.value, 'value: ')}`,
+    deleted: elem => `Property '${prefix}${elem.keyName}' was deleted`,
+    updated: elem => `Property '${prefix}${elem.keyName}' was updated. From ${printValue(elem.oldValue)} to ${printValue(elem.newValue)}`,
+    nested: elem => renderPlain(elem.value, `${elem.keyName}.`),
+  };
+
+  const fullPrefixedChildsAST = fp.map((node) => {
+    if (node.type === 'nested') {
+      return fp.map(child => ({ ...child, keyName: `${node.keyName}.${child.keyName}` }))(node.value);
+    }
+    return node;
+  })(ast);
+
+  const unnestedAST = fp.flatten(fullPrefixedChildsAST);
+  const astWithChangesOnly = fp.filter(node => node.type !== 'unchanged')(unnestedAST);
+  const result = fp.map(node => propertyActions[node.type](node))(astWithChangesOnly)
+    .join('\n');
+  return result;
+};
+
 export default (format) => {
   if (format === 'json') return renderJSON;
+  if (format === 'plain') return renderPlain;
   return () => 'unknown format';
 };
